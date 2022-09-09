a6b928fd5b503c1ce5713c51c2a29fc81c600a47
Deathik <zolotov2189@mail.ru>
2022-09-09 14:56:33 +0300

    Add pages catalog, product-page and finish footer also popup

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,6 +2,7 @@
 import { isMobile } from './functions.js';
 // Подключение списка активных модулей
 import { flsModules } from './modules.js';
+import IMask from 'imask';
 
 const language = document.querySelector('.select__body');
 const value = document.querySelectorAll('.select__option');
@@ -23,3 +24,84 @@ if (language) {
     clicked.classList.add('select__option--hide');
   });
 }
+
+// Validate form
+const form = document.forms['popup'];
+const inputs = document.querySelectorAll('.popup__input');
+const checkbox = document.querySelector('.popup__checkbox');
+const btn = document.querySelector('.popup__btn');
+const label = document.querySelectorAll('.popup__box');
+const thank = document.querySelector('.popup__thank');
+const boxCheck = document.querySelector('.popup__box-check');
+
+var element = document.getElementById('popup__phone');
+var maskOptions = {
+  mask: `{8}-(000)-000-00-00`,
+};
+var mask = IMask(element, maskOptions);
+
+const validFormArr = [];
+
+inputs.forEach(el => {
+  if (el.hasAttribute('data-reg')) {
+    el.setAttribute('is-valid', '0');
+    validFormArr.push(el);
+  }
+});
+
+form.addEventListener('input', function (e) {
+  const allValid = [];
+  inputs.forEach(el => {
+    allValid.push(el.getAttribute('is-valid'));
+  });
+  const isAllValid = allValid.every(inp => {
+    return inp === '1';
+  });
+
+  if (isAllValid && checkbox.checked) {
+    btn.classList.remove('popup__btn--disabled');
+    this.button.disabled = false;
+  } else {
+    this.button.disabled = true;
+    btn.classList.add('popup__btn--disabled');
+  }
+});
+
+Array.from(form.elements).forEach(inp => {
+  const inputData = inp.getAttribute('data-reg');
+  const re = new RegExp(inputData);
+  if (inp.required && inp.type != 'checkbox') {
+    inp.addEventListener('input', function () {
+      if (inp.hasAttribute('data-reg')) {
+        inp.setAttribute('is-valid', '0');
+        if (re.test(inp.value) && inp.checkValidity()) {
+          inp.previousElementSibling.style.color = 'green';
+          inp.setAttribute('is-valid', '1');
+        } else {
+          inp.previousElementSibling.style.color = 'red';
+          inp.setAttribute('is-valid', '0');
+          inp.reportValidity();
+        }
+      }
+    });
+  }
+});
+
+let click = 0;
+const thankYou = function (e) {
+  e.preventDefault();
+  if (thank.hasAttribute('hidden')) {
+    thank.removeAttribute('hidden');
+    label.forEach(el => {
+      el.style.display = 'none';
+    });
+    boxCheck.style.display = 'none';
+    this.textContent = 'Закрыть';
+  }
+  click++;
+  if (click === 2) {
+    this.setAttribute('data-close', '');
+  }
+};
+
+btn.addEventListener('click', thankYou);
--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -7,7 +7,7 @@
 // Подключаем слайдер Swiper из node_modules
 // При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
 // Пример: { Navigation, Autoplay }
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
 /*
 Основниые модули слайдера:
 Navigation, Pagination, Autoplay, 
@@ -34,7 +34,7 @@ function initSliders() {
       // Указываем скласс нужного слайдера
       // Подключаем модули слайдера
       // для конкретного случая
-      modules: [Navigation, Pagination],
+      modules: [Navigation, Pagination, Autoplay],
       observer: true,
       observeParents: true,
       slidesPerView: 1,
@@ -44,18 +44,16 @@ function initSliders() {
 
       //touchRatio: 0,
       //simulateTouch: false,
-      //loop: true,
+      loop: true,
       //preloadImages: false,
       //lazy: true,
 
-      /*
-			// Эффекты
-			effect: 'fade',
-			autoplay: {
-				delay: 3000,
-				disableOnInteraction: false,
-			},
-			*/
+      // Эффекты
+      effect: 'fade',
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
 
       // Пагинация
       pagination: {
